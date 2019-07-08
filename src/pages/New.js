b6699e2728f4e4eadef5d8484686f080cd8f4875
Luiz Menezes <lfmrod@gmail.com>
2019-07-08 18:19:52 -0300

    adicionando websocket

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,34 +1,75 @@
 import React, { Component } from 'react';
+import api from '../services/api';
 
 class New extends Component {
 
+    state = {
+        image: null,
+        author: '',
+        place: '',
+        description: '',
+        hashtags: '',
+    }
+
+    handleSubmit = async e => {
+        e.preventDefault();
+
+        const data = new FormData();
+
+        data.append('image', this.state.image);
+        data.append('author', this.state.author);
+        data.append('place', this.state.place);
+        data.append('description', this.state.description);
+        data.append('hashtags', this.state.hashtags);
+
+        await api.post('posts', data);
+        
+        this.props.history.push('/');
+    }
+
+    handleImageChange = e => {
+        this.setState({ image: e.target.files[0] });
+    }
+
+    handleChange = e => {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
     render() {
         return (
-            <form id="new-post">
-                <input type="file" />
+            <form id="new-post" onSubmit={this.handleSubmit} >
+                <input type="file" onChange={this.handleImageChange} />
 
                 <input
                     type="text"
                     name="author"
                     placeHolder="Autor do Post"
+                    onChange={this.handleChange}
+                    value={this.state.author}
                 />
 
                 <input
                     type="text"
                     name="place"
                     placeHolder="Local do Post"
+                    onChange={this.handleChange}
+                    value={this.state.place}
                 />
 
                 <input
                     type="text"
                     name="description"
                     placeHolder="Descrição do Post"
+                    onChange={this.handleChange}
+                    value={this.state.description}
                 />
 
                 <input
                     type="text"
                     name="hashtags"
                     placeHolder="Hashtags do Post"
+                    onChange={this.handleChange}
+                    value={this.state.hashtags}
                 />
 
                 <button type="submit">Enviar</button> 
